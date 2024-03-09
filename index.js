ca1caa3ba9035c169d1a31082514f221c3475b31
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2024-03-09 16:23:00 -0600

    started adding asteroid functionality

--- index.js
+++ index.js
@@ -34,6 +34,28 @@ class Player{
     }
 };
 
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position
+    this.velocity = velocity
+    this.radius = 5
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+    c.closePath()
+    c.fillStyle = 'white'
+    c.fill()
+  }
+
+  update() {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+  }
+}
+
 //add projectiles
 class Projectile{
     constructor({position, velocity}){
@@ -53,8 +75,30 @@ class Projectile{
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
-    }
+    };
 };
+
+class Asteroid {
+    constructor({ position, velocity, radius }) {
+      this.position = position
+      this.velocity = velocity
+      this.radius = radius
+    };
+  
+    draw() {
+      c.beginPath()
+      c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false)
+      c.closePath()
+      c.strokeStyle = 'white'
+      c.stroke()
+    };
+  
+    update() {
+      this.draw()
+      this.position.x += this.velocity.x
+      this.position.y += this.velocity.y
+    };
+  };
 
 //set player position to middle of screen 
 const player = new Player({
@@ -89,63 +133,113 @@ const PROJECTILE_SPEED = 3;
 const projectiles = [];
 
 //add animation function to continuously create player ship 
-function animate(){
-    window.requestAnimationFrame(animate);
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+function animate() {
+    const animationId = window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+  
     player.update();
-
-    player.velocity.x = 0;
-
-    if(keys.w.pressed) player.velocity.x = 1;
-
-    if (keys.d.pressed)player.rotation += 0.01;
-};
-
-
-
-
-//add event for key down controls
-window.addEventListener('keydown',(event)=>{
-    switch (event.code){
-        case 'KeyW':
-            keys.w.pressed = true;
-            console.log('w was pressed');
-            break
-        case 'KeyA':
-            keys.a.pressed = true;
-            console.log('a was pressed');
-            break
-        case 'KeyS':
-            keys.s.pressed = true;
-            console.log('s was pressed');
-            break
-        case 'KeyD':
-            keys.d.pressed = true;
-            console.log('d was pressed');
-            break
+  
+    for (let i = projectiles.length - 1; i >= 0; i--) {
+      const projectile = projectiles[i]
+      projectile.update();
+  
+      // garbage collection for projectiles
+      if (
+        projectile.position.x + projectile.radius < 0 ||
+        projectile.position.x - projectile.radius > canvas.width ||
+        projectile.position.y - projectile.radius > canvas.height ||
+        projectile.position.y + projectile.radius < 0
+      ) {
+        projectiles.splice(i, 1);
+      };
+    };
+  
+    // asteroid management
+    for (let i = asteroids.length - 1; i >= 0; i--) {
+      const asteroid = asteroids[i];
+      asteroid.update();
+  
+      if (circleTriangleCollision(asteroid, player.getVertices())) {
+        console.log('GAME OVER');
+        window.cancelAnimationFrame(animationId);
+        clearInterval(intervalId);
+      };
+  
+      // garbage collection for projectiles
+      if (
+        asteroid.position.x + asteroid.radius < 0 ||
+        asteroid.position.x - asteroid.radius > canvas.width ||
+        asteroid.position.y - asteroid.radius > canvas.height ||
+        asteroid.position.y + asteroid.radius < 0
+      ) {
+        asteroids.splice(i, 1);
+      };
+  
+      // projectiles
+      for (let j = projectiles.length - 1; j >= 0; j--) {
+        const projectile = projectiles[j]
+  
+        if (circleCollision(asteroid, projectile)) {
+          asteroids.splice(i, 1)
+          projectiles.splice(j, 1)
+        };
+      };
+    };
+    //add player rotation to controls
+    if (keys.w.pressed) {
+      player.velocity.x = Math.cos(player.rotation) * SPEED
+      player.velocity.y = Math.sin(player.rotation) * SPEED
+    } else if (!keys.w.pressed) {
+      player.velocity.x *= FRICTION
+      player.velocity.y *= FRICTION
     }
-});
+  
+    if (keys.d.pressed) player.rotation += ROTATIONAL_SPEED
+    else if (keys.a.pressed) player.rotation -= ROTATIONAL_SPEED
+  }
+  
+  animate()
+  //add keydown events to contols
+  window.addEventListener('keydown', (event) => {
+    switch (event.code) {
+      case 'KeyW':
+        keys.w.pressed = true
+        break
+      case 'KeyA':
+        keys.a.pressed = true
+        break
+      case 'KeyD':
+        keys.d.pressed = true
+        break
+      case 'Space':
+        projectiles.push(
+          new Projectile({
+            position: {
+              x: player.position.x + Math.cos(player.rotation) * 30,
+              y: player.position.y + Math.sin(player.rotation) * 30,
+            },
+            velocity: {
+              x: Math.cos(player.rotation) * PROJECTILE_SPEED,
+              y: Math.sin(player.rotation) * PROJECTILE_SPEED,
+            },
+          })
+        )
+        break
+    };
+  });
 
 //add event for keyup controls
-window.addEventListener('keyup',(event)=>{
-    switch (event.code){
-        case 'KeyW':
-            keys.w.pressed = false
-            console.log('w was pressed')
-            break
-        case 'KeyA':
-            keys.a.pressed = false
-            console.log('a was pressed')
-            break
-        case 'KeyS':
-            keys.s.pressed = false
-            console.log('s was pressed')
-            break
-        case 'KeyD':
-            keys.d.pressed = false
-            console.log('d was pressed')
-            break
-    }
-});
+window.addEventListener('keyup', (event) => {
+    switch (event.code) {
+      case 'KeyW':
+        keys.w.pressed = false
+        break
+      case 'KeyA':
+        keys.a.pressed = false
+        break
+      case 'KeyD':
+        keys.d.pressed = false
+        break
+    };
+  });
